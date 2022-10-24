b23980dde0fd4a66ec8332bdcd97c8ef81a4af9c
Félix Dorn <github@felixdorn.fr>
2022-10-24 09:55:52 +0200

    formats, types

--- app/Providers/EventServiceProvider.php
+++ app/Providers/EventServiceProvider.php
@@ -8,11 +8,7 @@ use Illuminate\Foundation\Support\Providers\EventServiceProvider as ServiceProvi
 
 class EventServiceProvider extends ServiceProvider
 {
-    /**
-     * The event to listener mappings for the application.
-     *
-     * @var array<class-string, array<int, class-string>>
-     */
+    /** @var array<class-string, array<int, class-string>> */
     protected $listen = [
         Registered::class => [
             SendEmailVerificationNotification::class,
