e331f290c942251be3a1f025f6e7ec707a4db4fd
Félix Dorn <github@felixdorn.fr>
2022-10-24 20:31:57 +0200

    switch to typescript

--- resources/js/app.ts
+++ resources/js/app.ts
@@ -10,7 +10,7 @@ createInertiaApp({
     title: (title) => `${title} - Laravel`,
     resolve: (name) => resolvePageComponent(`./Pages/${name}.vue`, import.meta.glob('./Pages/**/*.vue')),
     setup({el, app, props, plugin}) {
-        return createApp({render: () => h(app, props)})
+        createApp({render: () => h(app, props)})
             .use(plugin)
             .use(ZiggyVue)
             .mount(el);
--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -9,7 +9,7 @@
     <!-- Scripts -->
     @routes
     @vite('resources/css/app.css')
-    @vite('resources/js/app.js')
+    @vite('resources/js/app.ts')
     @inertiaHead
 </head>
 <body class="font-sans antialiased">
