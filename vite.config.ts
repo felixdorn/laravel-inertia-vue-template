a980b5db77b37cdd32969580d679e2967065b5f4
Félix Dorn <github@felixdorn.fr>
2022-10-29 09:17:38 +0200

    fix .gitignore

--- vite.config.ts
+++ vite.config.ts
@@ -16,6 +16,7 @@ export default defineConfig({
                     includeAbsolute: false,
                 },
             },
+            reactivityTransform: true
         }),
     ],
     resolve: {
